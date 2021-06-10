6499a59fd16a9aea0c1d674b72adfcb92088c65e
KamekuraMaisuke <taisukeoono350@gmail.com>
2021-06-10 13:10:17 +0900

    update get_zenn

--- script/get_zenn_article.sh
+++ script/get_zenn_article.sh
@@ -21,10 +21,19 @@ reponse_xml_format(){
 	grep -e '<title>' -e '<link>' -e '<pubDate>'	|
 	sed '1,4d' |
 	awk -F '[<>]' '
+	BEGIN{
+		split("Jan Feb Mar Apr May Jun Jul Aug Sep Oct Nov Dec",m," ")
+		for(i=1;i<=length(m);i++){
+			ma[m[i]]=i
+		}
+	}
 	{
 		if(NR%3){
 			ORS="\t"
 		}else {
+			gsub(/:/,"",$3)
+			split($3,da," ")
+			$3=sprintf("%04d/%02d/%02d/%06d",da[4],ma[da[3]],da[2],da[5])
 			ORS="\n"
 		}
 		print $3
